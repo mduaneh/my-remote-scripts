bc3c3418749ae4a71b65c59bb37efb401aff3f9c
Michael D. Hale <mhale@qualcomm.com>
2015-10-20 07:58:04 -0400

    With SSH locked down now, I need to route through the WIRED

--- Hale.sh
+++ Hale.sh
@@ -9,10 +9,11 @@ IP=$(host $Name 8.8.8.8 | grep "has address" | awk '{printf $NF}')
 Route=$(netstat  -rn | grep default | grep en0 | awk '{printf $2}')
 
 echo "Adding Route for ${Name} through en0(${Route})"
-sudo route -n add ${IP} ${Route}
+#sudo route -n add ${IP} ${Route}
 
 echo "Logging into ${WHO}'s Mac on port $RoutePort, vnc port $LocalPort"
-ssh -C ${User}@${IP} -p $RoutePort -L $LocalPort:localhost:5900 "$@"
+echo "\t ${User}@${Name}"
+ssh -vvv -C ${User}@${IP} -p $RoutePort -L $LocalPort:localhost:5900 "$@"
 echo "Removing Route for ${Name} through en0(${Route})"
-sudo route -n delete ${IP} ${Route}
+# sudo route -n delete ${IP} ${Route}
 
