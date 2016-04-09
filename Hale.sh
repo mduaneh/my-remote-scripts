a50de13a9e4130328f94ca289cf24112b7c3f06d
Michael D. Hale <mhale@qualcomm.com>
2016-04-09 08:23:21 -0400

    Updating Hale.sh no longer need to fix routes

--- Hale.sh
+++ Hale.sh
@@ -5,15 +5,16 @@ eval LocalPort=\$${WHO}LocalPort
 eval RoutePort=\$${WHO}RoutePort
 eval Name=\$${WHO}Name
 eval User=\$${WHO}User
+eval ExtraOptions=\$${WHO}ExtraOptions
 IP=$(host $Name 8.8.8.8 | grep "has address" | awk '{printf $NF}')
-Route=$(netstat  -rn | grep default | grep en0 | awk '{printf $2}')
+#Route=$(netstat  -rn | grep default | grep en0 | awk '{printf $2}')
 
-echo "Adding Route for ${Name} through en0(${Route})"
+#echo "Adding Route for ${Name} through en0(${Route})"
 #sudo route -n add ${IP} ${Route}
-
+echo "ExtraOptions: ${ExtraOptions}"
 echo "Logging into ${WHO}'s Mac on port $RoutePort, vnc port $LocalPort"
 echo "\t ${User}@${Name}"
-ssh -vvv -C ${User}@${IP} -p $RoutePort -L $LocalPort:localhost:5900 "$@"
-echo "Removing Route for ${Name} through en0(${Route})"
+ssh  -C ${User}@${IP} -p $RoutePort -L $LocalPort:localhost:5900 $ExtraOptions "$@"
+#echo "Removing Route for ${Name} through en0(${Route})"
 # sudo route -n delete ${IP} ${Route}
 
